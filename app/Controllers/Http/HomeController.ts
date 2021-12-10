982861e7f60da642468b6fc5230457480f5673ea
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-12-10 19:03:46 -0300

    Fluxo da Aplicação

---
+++ app/Controllers/Http/HomeController.ts
@@ -0,0 +1,11 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class HomeController {
+
+  async index() {
+
+    return 'Hello World'
+
+  }
+
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,3 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
-
-Route.post('/teste', async ({ view }) => {
-  return view.render('welcome')
-})
+Route.get('/', 'HomeController.index')
